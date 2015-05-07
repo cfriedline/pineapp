f9bf25190859bb3e7a49b82bffc554ab89be5eec
Chris Friedline <cfriedline@vcu.edu>
2015-05-07 14:31:02 -0400

    restrict to get for now

--- envdata/api.py
+++ envdata/api.py
@@ -6,10 +6,11 @@ class SampleResource(ModelResource):
     class Meta:
         queryset = Sample.objects.all()
         resource_name = 'sample'
+        allowed_methods = ['get'] 
 
 
 class PopulationResource(ModelResource):
     class Meta:
         queryset = Population.objects.all()
         resource_name = 'population'
-        
+        allowed_methods = ['get']
