f0c4dbc84ec0df95b7c0d9232fb1925686c49796
Chris Friedline <cfriedline@vcu.edu>
2015-05-07 13:53:59 -0400

    api start

--- pineapp/urls.py
+++ pineapp/urls.py
@@ -1,11 +1,16 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from envdata.api import SampleResource, PopulationResource
+from tastypie.api import Api
 
-urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'pineapp.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-    url(r'^grappelli/', include('grappelli.urls')), # grappelli URLS
-    url(r'^admin/', include(admin.site.urls)),
-    url(r'^$', include(admin.site.urls)),
-)
+sample_resource = SampleResource()
+population_resource = PopulationResource()
+
+v1_api = Api(api_name='v1')
+v1_api.register(sample_resource)
+v1_api.register(population_resource)
+
+urlpatterns = patterns('', url(r'^grappelli/', include('grappelli.urls')),
+                       url(r'^admin/', include(admin.site.urls)),
+                       url(r'^$', include(admin.site.urls)),
+                       url(r'^api/$', include(v1_api.urls)))
