be3e039190dd56e95211efa26d7b123bfa15836c
Chris Friedline <cfriedline@vcu.edu>
2015-05-07 13:52:27 -0400

    pep8

--- envdata/models.py
+++ envdata/models.py
@@ -1,11 +1,14 @@
 from django.db import models
 from django.contrib import admin
 from django.forms import ModelForm, TextInput
+from suit.widgets import SuitDateWidget, NumberInput
 
 # Create your models here.
 
+
 class Population(models.Model):
-    name = models.CharField(max_length=20,blank=False,null=False,unique=True)
+    name = models.CharField(max_length=20, blank=False, 
+                            null=False, unique=True)
     notes = models.TextField(blank=True, null=True)
 
     def __str__(self):
@@ -14,32 +17,47 @@ class Population(models.Model):
 
 class Sample(models.Model):
     population = models.ForeignKey(Population, null=True, blank=True)
-    name = models.CharField(max_length=20,blank=False,null=False,unique=True)
-    sample_date = models.DateField(null=True,blank=False)
-    lat = models.FloatField(blank=True,null=True)
-    lon = models.FloatField(blank=True,null=True)
-    dbh = models.FloatField(blank=True,null=True)
-    angle_low = models.FloatField(blank=True,null=True)
-    angle_high = models.FloatField(blank=True,null=True)
-    angle_distance = models.FloatField(blank=True,null=True)
-    height = models.FloatField(blank=True,null=True)
-    bark1 = models.FloatField(blank=True,null=True)
-    bark2 = models.FloatField(blank=True,null=True)
-    bark3 = models.FloatField(blank=True,null=True)
-    bark4 = models.FloatField(blank=True,null=True)
+    name = models.CharField(max_length=20, blank=False, null=False, 
+                            unique=True)
+    sample_date = models.DateField(null=True, blank=False)
+    lat = models.FloatField(blank=True, null=True)
+    lon = models.FloatField(blank=True, null=True)
+    dbh = models.FloatField(blank=True, null=True)
+    angle_low = models.FloatField(blank=True, null=True)
+    angle_high = models.FloatField(blank=True, null=True)
+    angle_distance = models.FloatField(blank=True, null=True)
+    height = models.FloatField(blank=True, null=True)
+    bark1 = models.FloatField(blank=True, null=True)
+    bark2 = models.FloatField(blank=True, null=True)
+    bark3 = models.FloatField(blank=True, null=True)
+    bark4 = models.FloatField(blank=True, null=True)
     notes = models.TextField(blank=True, null=True)
 
     def __str__(self):
         return self.name
 
+
+class SampleForm(ModelForm):
+    class Meta:
+        model = Sample
+        exclude = []
+        widgets = {
+            'sample_date': SuitDateWidget,
+        }
+
+
 class SampleInline(admin.StackedInline):
     model = Sample
     max_num = 15
-    extra=15
+    extra = 15
+
 
 class SampleAdmin(admin.ModelAdmin):
     list_display = ['name', 'population', 'sample_date']
+    form = SampleForm
+
 
 class PopulationAdmin(admin.ModelAdmin):
     inlines = [SampleInline]
     list_display = ['name','notes']
+    
