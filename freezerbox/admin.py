910c02f797fb79ea2704f50e5dcf0c9e57c6c444
Chris Friedline <cfriedline@vcu.edu>
2015-11-24 18:37:15 -0500

    remove freezerbox app

--- freezerbox/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from freezerbox.models import FreezerBox, FreezerBoxCell, FreezerBoxAdmin, FreezerBoxCellAdmin
-
-admin.site.register(FreezerBox, FreezerBoxAdmin)
-admin.site.register(FreezerBoxCell, FreezerBoxCellAdmin)
