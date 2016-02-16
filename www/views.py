876e757a2d6098d804216db5d4e198c6746c5300
Chris Friedline <chris@friedline.net>
2016-02-16 14:13:24 -0500

    format

--- www/views.py
+++ www/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render
 from django.contrib.gis.geos import Point
 from envdata.models import Population, Sample
+from django.core.urlresolvers import reverse
 
 
 def index(request):
     pops = Population.objects.all().order_by('name')
     samples = Sample.objects.all().order_by('name')
     context = {'pops': pops,
-               'samples': samples,
-               }
+               'samples': samples}
     return render(request, 'www/index.html', context)
