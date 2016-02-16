92a080eaacad3f90072dff98c12dce6714d6efaf
Chris Friedline <chris@friedline.net>
2016-02-16 13:49:21 -0500

    update to 1.9 format

--- www/urls.py
+++ www/urls.py
@@ -1,9 +1,8 @@
-from . import views
-from django.conf.urls import url, include, patterns
+from www import views
+from django.conf.urls import url
 from djgeojson.views import GeoJSONLayerView, TiledGeoJSONLayerView
 from envdata.models import Sample
 
-urlpatterns = patterns('',
-                       url(r'^data.geojson$', GeoJSONLayerView.as_view(model=Sample,
-                                                                       properties=['name','species']), name='data'),
-                       url(r'^$', views.index, name='index'), )
+urlpatterns = ['',
+               url(r'^data.geojson$', GeoJSONLayerView.as_view(model=Sample, properties=['name', 'species']), name='data'),
+               url(r'^$', views.index, name='index')]
