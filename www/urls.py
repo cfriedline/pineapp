d8b7c6089ea34455894264705b89a325dbe9bf1c
Chris Friedline <chris@friedline.net>
2018-02-07 22:59:46 -0500

    url fix?

--- www/urls.py
+++ www/urls.py
@@ -1,11 +1,11 @@
 
 
-from www import views
+import views
 from django.conf.urls import url
 from djgeojson.views import GeoJSONLayerView, TiledGeoJSONLayerView
 from envdata.models import Sample
 
-urlpatterns = [url(r'^data.geojson$/',
+urlpatterns = [url(r'^data.geojson$',
                GeoJSONLayerView.as_view(model=Sample,
                                         properties=['name', 'species']),
                name='data'),
